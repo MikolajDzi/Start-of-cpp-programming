890930a21274fbea8cdde27a05273ad254dba959
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-10-30 22:46:38 +0100

    merging strings, input whole sentences

--- Passion of Computer Science/Odcinek 8/Source.cpp
+++ Passion of Computer Science/Odcinek 8/Source.cpp
@@ -31,6 +31,18 @@ int main() {
 	for (int i = word_lenght -1; i>=0; i--)
 	{
 		cout << word[i];
-	}*/
+	}*/ //printing word backwards
+
+	/*string sentence;
+	cout << "Please give me the provisional sentence:";
+	getline(cin, sentence);
+	cout << sentence;*/ //getting whole sentences
+
+	/*string first_sentence = "Jenny has a cat ";
+	string second_sentence = "Toby has a dog ";
+	string merged_sentences = first_sentence + second_sentence;
+
+	cout << merged_sentences;*/
+
 	return 0;
 }
