7f325e7086d5595764d6923352af70dd79d82f8b
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-11-13 21:35:16 +0100

    Searching for the nearest number to average(beginning)

--- Passion of Computer Science/Odcinek 11/Source.cpp
+++ Passion of Computer Science/Odcinek 11/Source.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 //finding max value
 
-signed int a, b, c;
-int counter = 0;
+float a, b, c, d, e;
 
-int maximal(int a, int b, int c)
+/*signed int a, b, c;
+int counter = 0;
+float arr[5];
+
+/*int maximal(int a, int b, int c)
 {
 	int maxi=a;
 	for (int i = 0; i < counter; i++)
@@ -20,8 +23,12 @@ int maximal(int a, int b, int c)
 
 }
 
+
+
 int main()
 {
+	cout << "Please insert numbers: ";
+	
 	cout << "Please insert 3 numbers (with space after each): ";
 	cin >> a;
 	counter++;
@@ -30,14 +37,32 @@ int main()
 	cin >> c;
 	counter++;
 
-	/*if (a >= b && a >= c)
+	if (a >= b && a >= c)
 		cout << "Max value is a:" << a;
 	else if (b >= a && b >= c)
 		cout << "Max value is b: " << b;
 	else if (c >= b && c >= c)
-		cout << "Max value is c: " << c;*/
+		cout << "Max value is c: " << c;
 
 	cout << "Highest value is: " << maximal(a, b, c);
+
+	return 0;
+}*/
+
+float nearest_to_average(float a, float b, float c, float d, float e)
+{
+	float average = (a + b + c + d + e)/5;
+	float nearest_number=a;
+	if (a > average);
+
+}
+
+
+int main()
+{
+	cout << "Insert 5 numbers: ";
+	cin >> a >> b >> c >> d >> e;
+
 
 	return 0;
 }
