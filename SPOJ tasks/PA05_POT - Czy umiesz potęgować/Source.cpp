265817412e0bcf440c154f486bb15ab503b05d1b
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-27 23:28:18 +0100

    Two SPOJ tasks, sum and unity digit of power

--- SPOJ tasks/PA05_POT - Czy umiesz potęgować/Source.cpp
+++ SPOJ tasks/PA05_POT - Czy umiesz potęgować/Source.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int a, b, test, w=1;
+int a, b, test, w;
 int main()
 {
 	cin >> test;
 	for (int i = 0; i < test; i++)
-	{
+	{	
+		w = 1;
 		cin >> a >> b;
-		a %= 10;
+		a =a % 10;
 		for (int j = 0; j < (b - 1) % 4 + 1; j++)
 		{
 			w *= a;
 		}
-		cout << w << endl;
 		cout << w % 10<< endl;
 	}
 	return 0;
