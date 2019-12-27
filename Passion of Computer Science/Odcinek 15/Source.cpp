a44e121f38338b31840cf9082bb8a780f64fc7ef
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-27 17:00:08 +0100

    Polish lettter characters

---
+++ Passion of Computer Science/Odcinek 15/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+string phrase;
+
+
+int main()
+{
+
+	cout << "Za\276\242\210\206 g\251\230l\245 ja\253\344" << endl;
+	cin >> phrase;
+
+	cout << "Entered phrase(wpisany ci\245g znak\242w): "<< phrase << endl;
+	return 0;
+}
