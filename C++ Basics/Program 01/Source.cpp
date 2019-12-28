a5125c93852a0f5d7b459d2a6afe8a7fbdb75430
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-28 21:45:56 +0100

    Revision of C++ basics

---
+++ C++ Basics/Program 01/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello in the revision of C++ basics. \n";
+	cout << 2 + 2 << "\n";
+	cout << endl;
+	cout << 2 + 5;
+	return 0;
+}
