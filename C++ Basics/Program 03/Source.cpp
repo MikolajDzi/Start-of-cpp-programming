e8f926150ca974b04caa1f5d7a4a010a1e508e19
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-29 17:36:25 +0100

    Basics of creating names  for variables etc.

---
+++ C++ Basics/Program 03/Source.cpp
@@ -0,0 +1,18 @@
+//IDs
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	We can use specific characters, like polish special characters,
+	use different names for variables and functions,
+	try to use names which mean something,
+	f.ex.
+	Shopingcart - correct
+	mal11234 - correct but doesn't tell anything about the content
+	sumaShopingcart - correct
+	s_shopingcart - correct
+	*/
+	return 0;
+}
