3e11af9164a9e21cc13fa02faa1a8ee6eb8c6b7b
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-29 17:29:39 +0100

    Revision, comments

---
+++ C++ Basics/Program 02/Source.cpp
@@ -0,0 +1,16 @@
+//Comments
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Example of one verse comment
+	//Output of the sentence 'cout' -> character output, by default screen is our output register
+	cout << "Welcome in the revision of C++ basics";
+
+	/*
+	Example of multiverse comments
+	*/
+	return 0;
+}
