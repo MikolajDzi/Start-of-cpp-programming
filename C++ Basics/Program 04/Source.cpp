6c00152c4dc55cd6de1f9dcb73c660b066ba41e9
MikolajDzi <mikolaj.dziubinski@interia.pl>
2019-12-29 17:51:45 +0100

    Basics of variables

---
+++ C++ Basics/Program 04/Source.cpp
@@ -0,0 +1,20 @@
+//Variables
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number_of_elements;
+	int sec_number_of_elements;
+
+	number_of_elements = 12;
+	sec_number_of_elements = 22;
+	cout <<"First number of elements: " << number_of_elements << endl;
+	cout << "Second number of elements: " << sec_number_of_elements << endl;
+	cout << "Sum of first and second number: " << number_of_elements + sec_number_of_elements  << endl;
+	sec_number_of_elements = 1000;
+	cout << "First number of elements: " << number_of_elements << endl;
+	cout << "Second number of elements: " << sec_number_of_elements << endl;
+	cout << "Sum of first and second number: " << number_of_elements + sec_number_of_elements << endl;
+	return 0;
+}
